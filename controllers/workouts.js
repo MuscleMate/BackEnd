a4f5bb05e2c6b8d27cb07d13edf783a869486da8
Kamil Magdziarz <kmkmagdziarz@gmail.com>
2024-03-08 17:10:48 +0100

    changes

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -1,7 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const Workout = require("../models/Workout");
-const User = require("../models/User");
-
+const User = require("../models/User")
 const add_workout = async (req, res)=>{
     try{
         const workout = new Workout(req.body);
@@ -19,14 +18,24 @@ const add_workout = async (req, res)=>{
     }
 }
 
-const get_workout = async (req, res)=>{
-    try{
-        const user = new User(req.body);
-        res.status(StatusCodes.OK).json(user.workouts);
-    }
-    catch(err){
-        res.status(StatusCodes.BAD_REQUEST).json(err);
+const get_workout = async (req, res) => {
+    try {
+        const userId = req.query.userID;
+        if (!userId) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'User ID is required' });
+        }
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'User not found' });
+        }
+
+        // You may return the entire user object or specific fields as needed
+        res.status(StatusCodes.OK).json({ user });
+    } catch (err) {
+        console.error(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Server error' });
     }
 }
 
-module.exports = { add_workout,get_workout};
+module.exports = { add_workout, get_workout};
