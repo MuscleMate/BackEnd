3cc8e51413bc1ed2c4ef784febb1d681af2e1436
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-03-08 16:18:32 +0100

    deleted name field from UserScheme

--- models/User.js
+++ models/User.js
@@ -2,12 +2,6 @@ const mongoose = require("mongoose");
 const validator = require("validate");
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name"],
-    minlength: 3,
-    maxlength: 50,
-  },
   email: {
     type: String,
     unique: true,
