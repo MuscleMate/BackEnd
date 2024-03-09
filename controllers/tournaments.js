60c1d97670b8b65cb5434dfb085e2efc3c22f0d9
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-03-09 17:17:08 +0100

    small changes to get_tournaments

--- controllers/tournaments.js
+++ controllers/tournaments.js
@@ -1,13 +1,17 @@
 const { StatusCodes } = require("http-status-codes");
-const User = require("../models/user.js");
+const User = require("../models/User");
 
 const getTournaments = async (req, res) => {
     try {
-        const user = await User.findById(req.query.userID);
+        const user = await User.findById(req.body.user);
+
         if (!user) {
             return res.status(StatusCodes.NOT_FOUND).json({ message: 'User not found' });
         }
-        res.status(StatusCodes.OK).json(user.tournaments);
+        const tournaments = await user.populate("tournaments");
+        res.status(StatusCodes.OK).json({ tournaments: tournaments.tournaments });
+        res.status(StatusCodes.OK).json({});
+
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({ error: 'Failed to retrieve tournaments' });
     }
