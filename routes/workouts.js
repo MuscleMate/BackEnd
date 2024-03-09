36a59674056d63cf8ad7363c19f13b32832d5307
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-03-09 15:45:03 +0100

    small changes in get_workouts

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const { add_workout, get_workout } = require("../controllers/workouts.js");
+const { add_workout, get_workouts } = require("../controllers/workouts.js");
 
 router.route("/").post(add_workout);
-router.route("/").get(get_workout);
+router.route("/").get(get_workouts);
 module.exports = router;
