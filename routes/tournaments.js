904833edc5919c8187652960d124b82b39733fad
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-03-12 18:41:18 +0100

    added updateTournament request

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getTournaments, createTournament } = require("../controllers/tournaments.js");
+const { getTournaments, createTournament, updateTournament } = require("../controllers/tournaments.js");
 
 // GET route to retrieve tournaments data for a specified user
 router.route("/").get(getTournaments).post(createTournament);
+router.route("/:id").put(updateTournament);
 
 module.exports = router;
