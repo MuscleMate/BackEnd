04d4c9c371aa75045a6a9bb15dc4eef47a4530a3
Jakub Kostrzewa <kubacfaniak@gmail.com>
2024-04-02 21:08:20 +0200

    Add POST add friend request

--- controllers/friends.js
+++ controllers/friends.js
@@ -20,4 +20,31 @@ const getFriends = async(req,res) => {
     } 
 }
 
-module.exports ={getFriends};
+const addFriend = async(req,res) =>{
+    const {userID} = req.body;
+    const { user:_id } = req.body;
+    const user = await User.findById(_id);
+    const userToBeAdded = await User.findById(userID);
+    if(!user)
+    {
+        throw new NotFoundError('User does not exist');
+    }
+    if(!userToBeAdded)
+    {
+        throw new NotFoundError('User to be added does not exist');
+    }
+    if(user.receivedFriendsRequests.indexOf(userToBeAdded._id)===-1)
+    {
+        throw new NotFoundError('User to be added did not send a request');
+    }
+    try{
+        await userToBeAdded.updateOne({ $push: { friends: user._id} });
+        await user.updateOne({ $push: { friends: userToBeAdded._id} });
+        await user.updateOne({ $pull: { receivedFriendsRequests: userToBeAdded._id} });
+        await userToBeAdded.updateOne({ $pull: { sentFriendsRequests: user._id} });
+    } catch(error){
+        throw new BadRequestError(error.message);
+    }
+}
+
+module.exports ={getFriends,addFriend};
