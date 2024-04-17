a3fc2e8558edd40e8ef91095c62d69f18e7373d3
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-04-17 18:40:42 +0200

    Refactor sendNotification function to accept multiple receivers

--- utils/sendNotification.js
+++ utils/sendNotification.js
@@ -1,19 +1,25 @@
 const { BadRequestError } = require("../errors");
 const User = require("../models/User");
 
-const sendNotification = async (senderID, receiverID, message) => {
+/** Sends a notification to a user
+ * @param {string} senderID - the id of the user who sends the notification
+ * @param {string[]} receivers - the ids of the users who will receive the notification
+ * @param {string} message - the message of the notification
+ */
+const sendNotification = async (senderID, receivers, message) => {
   const sender = User.findById(senderID);
   if (!sender) {
     throw BadRequestError(`User with id ${senderID} not found`);
   }
-
-  const receiver = User.findById(receiverID);
-  if (!receiver) {
-    throw BadRequestError(`User with id ${receiverID} not found`);
-  }
-
+  
   try {
-    await receiver.updateOne({ $push: { notifications: { senderID, receiverID, message } } });
+    receivers.forEach(async (receiverID) => {
+      const receiver = User.findById(receiverID);
+      if (!receiver) {
+        throw BadRequestError(`User with id ${receiverID} not found`);
+      }
+      await receiver.updateOne({ $push: { notifications: { senderID, receiverID, message } } });
+    })
   } catch (err) {
     throw new BadRequestError(err);
   }
