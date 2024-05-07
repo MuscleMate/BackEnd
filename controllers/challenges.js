559b7db92942671c9005b2b06337e42db2ed97bb
Jakub Kostrzewa <kubacfaniak@gmail.com>
2024-05-07 16:50:19 +0200

    Add replaceChallenge

--- controllers/challenges.js
+++ controllers/challenges.js
@@ -1,4 +1,5 @@
 const {StatusCodes} = require('http-status-codes');
+const Challenge = require('../models/Challenge');
 
 /** Get user's challenges
  * Checks if any of current challenges are expired, if so deletes them and draws a new ones with the same difficulty level
@@ -25,7 +26,33 @@ const getChallenges = async (req, res) => {
  */
 const replaceChallenge = async (req, res) => {
     //TODO delete deprecated tag after implementing the function
-    res.status(StatusCodes.NOT_IMPLEMENTED).json({message: "Not implemented"});
+    const{ challengeID } = req.body;
+    const{ user:userID } = req.body;
+    challengeToChange = await Challenge.findById(challengeID);
+    if(!challengeToChange)
+    {
+        throw new NotFoundError(`No challenge with id : ${challengeID}`);
+    }
+    const user = await User.findById(userID);
+    if (!user) 
+    {
+        throw new NotFoundError('User does not exist');
+    }
+    if(user.challenges.indexOf(challengeToChange)===-1)
+    {
+        throw new UnauthorizedError('User not authorized to replace this challenge');
+    }
+    difficulty = challengeToChange.difficulty;
+    try{
+        const challengeReplacment = await ChallengesList.find({difficulty : difficulty});
+        await user.updateOne({ $pull: { challenges: challengeToChange } });
+        await user.updateOne({ $push: { challenges: challengeReplacment } });
+        res.status(StatusCodes.OK).json(challengeReplacment);
+    }
+    catch(err){
+        throw new BadRequestError(err);
+    }
+    
 };
 
 module.exports = {
