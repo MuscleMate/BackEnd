5f1d102be3757960044e9d803d2d8b0d5de46718
Jakub Kostrzewa <kubacfaniak@gmail.com>
2024-05-14 13:11:27 +0200

    Implement getChallenges controller Fixes #44

--- controllers/challenges.js
+++ controllers/challenges.js
@@ -1,4 +1,8 @@
 const {StatusCodes} = require('http-status-codes');
+const Challenge = require('../models/Challenge');
+const User = require('../models/User');
+const { BadRequestError, NotFoundError} = require('../errors');
+const drawChallege = require('../utils/challenges');
 
 /** Get user's challenges
  * Checks if any of current challenges are expired, if so deletes them and draws a new ones with the same difficulty level
@@ -7,12 +11,42 @@ const {StatusCodes} = require('http-status-codes');
  * @url GET /challenges 
  * @query count - number of past challenges to return
  * @response challenges - list of current and past challenges
- * @deprecated not implemented
  */
 const getChallenges = async (req, res) => {
+    let {count} = req.query;
+    const{ user:userID } = req.body;
+    const user = await User.findById(userID).populate({
+        path:'challenges',
+        mode:'Challenge',
+        select:'_id title description startDate endDate difficulty status'
+    }).limit(count);
+    if(!user)
+    {
+        throw new NotFoundError(`No user with id : ${userID}`);
+    }
+    try
+    {
+        for(challange in user.challenges)
+        {
+            if(challange.status == 'ongoing' && challange.endDate < Date.now)
+            {
+                difficulty = challange.difficulty;
+                const challengeReplacment = await drawChallege(difficulty);
+                await user.updateOne({ $pull: { challenges: challenge._id } });
+                await user.updateOne({ $push: { challenges: challengeReplacment._id } });
+                await Challenge.deleteOne({ _id: challengeID });
+            }
+        }
+        res.status(StatusCodes.OK).json({challenges: user.challenges });
+    }
+    catch(err)
+    {
+            throw new BadRequestError(err.message);
+    }
+    
     //TODO delete deprecated tag after implementing the function
     //TODO @VEXI19 calculating exp granted for completing challenge
-    res.status(StatusCodes.NOT_IMPLEMENTED).json({message: "Not implemented"});
+
 }
 
 /** Replace given challenge with a new one
