508971c442e74963a92bdbe6d333b7c2793dbdb5
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-05-16 14:53:06 +0200

    Implement ranking requests

--- controllers/friends.js
+++ controllers/friends.js
@@ -8,7 +8,7 @@ const sendNotification = require("../utils/sendNotification");
  * @body user - the user id
  * @response friends - array of friends
  */
-const getFriends = async(req,res) => {
+const getFriends = async (req, res) => {
     try {
         const user = await User.findById(req.body.user).populate({
             path: "friends",
@@ -21,7 +21,7 @@ const getFriends = async(req,res) => {
         res.status(StatusCodes.OK).json({ friends: user.friends });
     } catch (error) {
         throw new BadRequestError(error.message);
-    } 
+    }
 }
 
 /** Send a friend request
@@ -30,40 +30,35 @@ const getFriends = async(req,res) => {
  * @body user - the id of the user sending the request
  * @response 204 - No content
  */
-const sendRequest = async(req,res) =>{
-    const {id} = req.body;
-    const { user:userID } = req.body;
+const sendRequest = async (req, res) => {
+    const { id } = req.body;
+    const { user: userID } = req.body;
     const user = await User.findById(userID);
     const userToBeAdded = await User.findById(id);
-    if(!user)
-    {
+    if (!user) {
         throw new NotFoundError('User does not exist');
     }
-    if(!userToBeAdded)
-    {
+    if (!userToBeAdded) {
         throw new NotFoundError('User to be added does not exist');
     }
     if (user._id.toString() === userToBeAdded._id.toString()) {
         throw new BadRequestError('You cannot send a friend request to yourself');
     }
-    if(user.friends.indexOf(userToBeAdded._id)!==-1)
-    {
+    if (user.friends.indexOf(userToBeAdded._id) !== -1) {
         throw new BadRequestError('User is already a friend');
     }
-    if(user.sentFriendsRequests.indexOf(userToBeAdded._id)!==-1)
-    {
+    if (user.sentFriendsRequests.indexOf(userToBeAdded._id) !== -1) {
         throw new BadRequestError('Invitation has already been sent');
     }
-    if(user.receivedFriendsRequests.indexOf(userToBeAdded._id)!==-1)
-    {
+    if (user.receivedFriendsRequests.indexOf(userToBeAdded._id) !== -1) {
         throw new BadRequestError('You have already received a friend request from this user');
     }
-    try{
-        await userToBeAdded.updateOne({ $push: { receivedFriendsRequests: user._id} });
-        await user.updateOne({ $push: { sentFriendsRequests: userToBeAdded._id} });
+    try {
+        await userToBeAdded.updateOne({ $push: { receivedFriendsRequests: user._id } });
+        await user.updateOne({ $push: { sentFriendsRequests: userToBeAdded._id } });
         await sendNotification([userToBeAdded._id], `${user.firstName} sent you a friend request`);
         res.status(StatusCodes.NO_CONTENT).json({});
-    }catch(error){
+    } catch (error) {
         throw new BadRequestError(error.message);
     }
 }
@@ -74,31 +69,28 @@ const sendRequest = async(req,res) =>{
  * @body user - the id of the user accepting the request
  * @response 204 - No content
  */
-const addFriend = async(req,res) =>{
-    const {id} = req.body;
-    const { user:userID } = req.body;
+const addFriend = async (req, res) => {
+    const { id } = req.body;
+    const { user: userID } = req.body;
     const user = await User.findById(userID);
     const userToBeAdded = await User.findById(id);
-    if(!user)
-    {
+    if (!user) {
         throw new NotFoundError('User does not exist');
     }
-    if(!userToBeAdded)
-    {
+    if (!userToBeAdded) {
         throw new NotFoundError('User to be added does not exist');
     }
-    if(user.receivedFriendsRequests.indexOf(userToBeAdded._id)===-1)
-    {
+    if (user.receivedFriendsRequests.indexOf(userToBeAdded._id) === -1) {
         throw new NotFoundError('User to be added did not send a request');
     }
-    try{
-        await userToBeAdded.updateOne({ $push: { friends: user._id} });
-        await user.updateOne({ $push: { friends: userToBeAdded._id} });
-        await user.updateOne({ $pull: { receivedFriendsRequests: userToBeAdded._id} });
-        await userToBeAdded.updateOne({ $pull: { sentFriendsRequests: user._id} });
+    try {
+        await userToBeAdded.updateOne({ $push: { friends: user._id } });
+        await user.updateOne({ $push: { friends: userToBeAdded._id } });
+        await user.updateOne({ $pull: { receivedFriendsRequests: userToBeAdded._id } });
+        await userToBeAdded.updateOne({ $pull: { sentFriendsRequests: user._id } });
         await sendNotification([userToBeAdded._id], `${user.firstName} accepted your friend request`);
         res.status(StatusCodes.NO_CONTENT).json({});
-    } catch(error){
+    } catch (error) {
         throw new BadRequestError(error.message);
     }
 }
@@ -109,28 +101,25 @@ const addFriend = async(req,res) =>{
  * @body user - the id of the user canceling the request
  * @response 204 - No content
  */
-const cancelFriend = async(req,res) =>{
-    const{id} = req.body;
-    const { user:userID } = req.body;
+const cancelFriend = async (req, res) => {
+    const { id } = req.body;
+    const { user: userID } = req.body;
     const user = await User.findById(userID);
     const userToBeCanceled = await User.findById(id);
-    if(!user)
-    {
+    if (!user) {
         throw new NotFoundError('User does not exist');
     }
-    if(!userToBeCanceled)
-    {
+    if (!userToBeCanceled) {
         throw new NotFoundError('User to be denyed friend request does not exist');
     }
-    if(user.sentFriendsRequests.indexOf(userToBeCanceled._id)===-1)
-    {
+    if (user.sentFriendsRequests.indexOf(userToBeCanceled._id) === -1) {
         throw new NotFoundError('User did not send a friend request to that user');
     }
-    try{
-        await user.updateOne({ $pull: { sentFriendsRequests: userToBeCanceled._id} });
-        await userToBeCanceled.updateOne({ $pull: { receivedFriendsRequests: user._id} });
+    try {
+        await user.updateOne({ $pull: { sentFriendsRequests: userToBeCanceled._id } });
+        await userToBeCanceled.updateOne({ $pull: { receivedFriendsRequests: user._id } });
         res.status(StatusCodes.NO_CONTENT).json({});
-    } catch(error){
+    } catch (error) {
         throw new BadRequestError(error.message);
     }
 }
@@ -141,30 +130,27 @@ const cancelFriend = async(req,res) =>{
  * @body user - the id of the user deleting the friend
  * @response 204 - No content
  */
-const deleteFriend = async(req,res) =>{
-    const{id} = req.body;
-    const { user:userID } = req.body;
+const deleteFriend = async (req, res) => {
+    const { id } = req.body;
+    const { user: userID } = req.body;
     const user = await User.findById(userID);
     const userToDeleted = await User.findById(id);
-    if(!user)
-    {
+    if (!user) {
         throw new NotFoundError('User does not exist');
     }
 
-    if(!userToDeleted)
-    {
+    if (!userToDeleted) {
         throw new NotFoundError('User to be deleted  does not exist');
     }
-    if(user.friends.indexOf(userToDeleted._id)===-1)
-    {
+    if (user.friends.indexOf(userToDeleted._id) === -1) {
         throw new NotFoundError('User to be deleted from friend list is not a friend');
     }
-    try{
-        await user.updateOne({ $pull: { friends: userToDeleted._id} });
-        await userToDeleted.updateOne({ $pull: { friends: user._id} });
+    try {
+        await user.updateOne({ $pull: { friends: userToDeleted._id } });
+        await userToDeleted.updateOne({ $pull: { friends: user._id } });
 
         res.status(StatusCodes.NO_CONTENT).json({});
-    } catch(error){
+    } catch (error) {
         throw new BadRequestError(error.message);
     }
 }
@@ -175,28 +161,25 @@ const deleteFriend = async(req,res) =>{
  * @body user - the id of the user denying the request
  * @response 204 - No content
  */
-const denyFriend = async(req,res) =>{
-    const{id} = req.body;
-    const { user:userID } = req.body;
+const denyFriend = async (req, res) => {
+    const { id } = req.body;
+    const { user: userID } = req.body;
     const user = await User.findById(userID);
     const userToDenyed = await User.findById(id);
-    if(!user)
-    {
+    if (!user) {
         throw new NotFoundError('User does not exist');
     }
-    if(!userToDenyed)
-    {
+    if (!userToDenyed) {
         throw new NotFoundError('User to be denyed friend request does not exist');
     }
-    if(user.receivedFriendsRequests.indexOf(userToDenyed._id)===-1)
-    {
+    if (user.receivedFriendsRequests.indexOf(userToDenyed._id) === -1) {
         throw new NotFoundError('User to be denyed friend did not send a request');
     }
-    try{
-        await user.updateOne({ $pull: { receivedFriendsRequests: userToDenyed._id} });
-        await userToDenyed.updateOne({ $pull: { sentFriendsRequests: user._id} });
+    try {
+        await user.updateOne({ $pull: { receivedFriendsRequests: userToDenyed._id } });
+        await userToDenyed.updateOne({ $pull: { sentFriendsRequests: user._id } });
         res.status(StatusCodes.NO_CONTENT).json({});
-    } catch(error){
+    } catch (error) {
         throw new BadRequestError(error.message);
     }
 }
@@ -219,7 +202,7 @@ const getReceivedRequests = async (req, res) => {
         res.status(StatusCodes.OK).json({ receivedFriendsRequests: user.receivedFriendsRequests });
     } catch (error) {
         throw new BadRequestError(error.message);
-    } 
+    }
 }
 
 /** Get sent friend requests
@@ -240,7 +223,7 @@ const getSentRequests = async (req, res) => {
         res.status(StatusCodes.OK).json({ sentFriendsRequests: user.sentFriendsRequests });
     } catch (error) {
         throw new BadRequestError(error.message);
-    } 
+    }
 }
 
 /** Get level ranking
@@ -260,7 +243,7 @@ const getLevelRanking = async (req, res) => {
             throw new NotFoundError(`User with id ${userID} not found`);
         }
 
-        let ranking = [...user.friends, {_id: userID, RP: user.RP, firstName: user.firstName, lastName: user.lastName}]
+        let ranking = [...user.friends, { _id: userID, RP: user.RP, firstName: user.firstName, lastName: user.lastName }]
 
         ranking = ranking.sort((a, b) => b.RP.level - a.RP.level);
         res.status(StatusCodes.OK).json({ ranking });
@@ -286,7 +269,7 @@ const getExpRanking = async (req, res) => {
             throw new NotFoundError(`User with id ${userID} not found`);
         }
 
-        let ranking = [...user.friends, {_id: userID, RP: user.RP, firstName: user.firstName, lastName: user.lastName}]
+        let ranking = [...user.friends, { _id: userID, RP: user.RP, firstName: user.firstName, lastName: user.lastName }]
 
         ranking = ranking.sort((a, b) => b.RP.totalPoints - a.RP.totalPoints);
         res.status(StatusCodes.OK).json({ ranking });
@@ -299,11 +282,29 @@ const getExpRanking = async (req, res) => {
  * @url GET /friends/rankings/challenges
  * @body user - the id of the user
  * @response ranking - array of friends and user sorted by challenges won
- * @deprecated Not implemented
  */
 const getChallengesRanking = async (req, res) => {
-    // TODO: delete deprecated tag from JSDoc when implemented
-    res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
+    const { user: userID } = req.body;
+
+    try {
+        const user = await User.findById(userID).populate({
+            path: "friends",
+            select: ["firstName", "lastName", "_id", "stats.challengesCompleted"]
+        })
+        if (!user) {
+            throw new NotFoundError(`User with id ${userID} not found`);
+        }
+
+        const friends = user.friends.map(friend => {
+            return { _id: friend._id, firstName: friend.firstName, lastName: friend.lastName, challengesCompleted: friend.stats.challengesCompleted }
+        })
+
+        let ranking = [...friends, { _id: userID, firstName: user.firstName, lastName: user.lastName, challengesCompleted: user.stats.challengesCompleted }]
+        ranking = ranking.sort((a, b) => b.challengesCompleted - a.challengesCompleted);
+        res.status(StatusCodes.OK).json({ ranking });
+    } catch (error) {
+        throw new BadRequestError(error.message);
+    }
 }
 
 /** Get workouts ranking
@@ -317,15 +318,18 @@ const getWorkoutsRanking = async (req, res) => {
     try {
         const user = await User.findById(userID).populate({
             path: "friends",
-            select: ["firstName", "lastName", "_id", "RP", "stats"]
+            select: ["firstName", "lastName", "_id", "stats.workoutsCompleted"]
         })
         if (!user) {
             throw new NotFoundError(`User with id ${userID} not found`);
         }
 
-        let ranking = [...user.friends, {_id: userID, RP: user.RP, firstName: user.firstName, lastName: user.lastName}]
+        const friends = user.friends.map(friend => {
+            return { _id: friend._id, firstName: friend.firstName, lastName: friend.lastName, workoutsCompleted: friend.stats.workoutsCompleted }
+        })
 
-        ranking = ranking.sort((a, b) => b.stats.workoutsCompleted - a.stats.workoutsCompleted);
+        let ranking = [...friends, { _id: userID, firstName: user.firstName, lastName: user.lastName, workoutsCompleted: user.stats.workoutsCompleted }]
+        ranking = ranking.sort((a, b) => b.workoutsCompleted - a.workoutsCompleted);
         res.status(StatusCodes.OK).json({ ranking });
     } catch (error) {
         throw new BadRequestError(error.message);
@@ -343,15 +347,19 @@ const getTournamentsRanking = async (req, res) => {
     try {
         const user = await User.findById(userID).populate({
             path: "friends",
-            select: ["firstName", "lastName", "_id", "RP", "stats"]
+            select: ["firstName", "lastName", "_id", "stats.tournamentsWon"]
         })
         if (!user) {
             throw new NotFoundError(`User with id ${userID} not found`);
         }
 
-        let ranking = [...user.friends, {_id: userID, RP: user.RP, firstName: user.firstName, lastName: user.lastName}]
+        const friends = user.friends.map(friend => {
+            return { _id: friend._id, firstName: friend.firstName, lastName: friend.lastName, tournamentWins: friend.stats.tournamentsWon }
+        })
 
-        ranking = ranking.sort((a, b) => b.stats.tournamentsWon - a.stats.tournamentsWon);
+        let ranking = [...friends, { _id: userID, firstName: user.firstName, lastName: user.lastName, tournamentWins: user.stats.tournamentsWon }]
+
+        ranking = ranking.sort((a, b) => b.tournamentWins - a.tournamentWins);
         res.status(StatusCodes.OK).json({ ranking });
     } catch (error) {
         throw new BadRequestError(error.message);
@@ -364,12 +372,12 @@ const getTournamentsRanking = async (req, res) => {
  * @query count - number of friends to return
  * @response friends
  */
-const searchFriend = async(req,res) =>{
+const searchFriend = async (req, res) => {
     const { searchText } = req.body;
     let { count } = req.query;
 
     count = count ? count : 10;
-    
+
     try {
         const user = await User.findById(req.body.user).populate({
             path: "friends",
@@ -383,9 +391,9 @@ const searchFriend = async(req,res) =>{
             select: ["firstName", "lastName", "email", "_id"],
             match: {
                 $or: [
-                    {firstName: {$regex: searchText, $options: 'i'}},
-                    {lastName: {$regex: searchText, $options: 'i'}},
-                    {email: {$regex: searchText, $options: 'i'}}
+                    { firstName: { $regex: searchText, $options: 'i' } },
+                    { lastName: { $regex: searchText, $options: 'i' } },
+                    { email: { $regex: searchText, $options: 'i' } }
                 ]
             },
             limit: count
@@ -397,6 +405,8 @@ const searchFriend = async(req,res) =>{
     }
 }
 
-module.exports ={getFriends,addFriend, sendRequest, cancelFriend,denyFriend, deleteFriend, getReceivedRequests, getSentRequests, 
-    getLevelRanking, getExpRanking, getChallengesRanking, getWorkoutsRanking, getTournamentsRanking, searchFriend };
+module.exports = {
+    getFriends, addFriend, sendRequest, cancelFriend, denyFriend, deleteFriend, getReceivedRequests, getSentRequests,
+    getLevelRanking, getExpRanking, getChallengesRanking, getWorkoutsRanking, getTournamentsRanking, searchFriend
+};
 
